4065f8e7b0f925386f6a1ed44c8b218d828c4b74
Franccesco <101402711+Franccescoo@users.noreply.github.com>
2026-05-13 12:28:16 -0400

    abse

---
+++ reserva_sala_junta/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'reserva_sala_junta',
+  webDir: 'www'
+};
+
+export default config;
